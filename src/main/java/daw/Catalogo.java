19b5b54ce8cecbecf37a4afa06a63ac7a57e0f67
AdrianViana <adrianvianagarcia@gmail.com>
2024-01-14 19:51:34 +0100

    Todos los metodos añadidos, sin probar en main

--- src/main/java/daw/Catalogo.java
+++ src/main/java/daw/Catalogo.java
@@ -1,6 +1,7 @@
 package daw;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Catalogo {
 
@@ -49,4 +50,35 @@ public class Catalogo {
         lista.remove(c1);
     }
 
+    public void eliminarElementos(ArrayList<Catalogo> listaBorrar) {
+        lista.remove(listaBorrar);
+    }
+
+    public void eliminarTodos() {
+        lista.clear();
+    }
+
+    public void imprimirCatalogo() {
+        lista.forEach(System.out::println);
+    }
+
+    public int buscarELemento(Coches c1) {
+        return lista.indexOf(c1);
+    }
+
+    public void ordenarPorCaballos() {
+        Collections.sort(lista, (e1, e2) -> Integer.compare(e1.getCaballos(),
+                e2.getCaballos()));
+    }
+
+    public void ordenarPorMarca() {
+        Collections.sort(lista, (e1, e2) -> e1.getMarca().
+                compareToIgnoreCase(e2.getMarca()));
+    }
+
+    public int buscarElemento(Coches c1) {
+
+        return Collections.binarySearch(lista, c1, (k1, k2) -> k1.getMarca().
+                compareToIgnoreCase(k2.getMarca()));
+    }
 }
